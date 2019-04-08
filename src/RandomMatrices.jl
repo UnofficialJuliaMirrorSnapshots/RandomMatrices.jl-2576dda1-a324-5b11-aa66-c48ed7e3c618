b8722d627472e8075b62c0a3c83eddc347ce51e3
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:58:56 +0000

    Snapshot of branch master taken on 2019-04-07T21:58:55.254-04:00 from "https://github.com/UnofficialJuliaMirror/RandomMatrices.jl-2576dda1-a324-5b11-aa66-c48ed7e3c618"

---
+++ src/RandomMatrices.jl
@@ -0,0 +1,61 @@
+module RandomMatrices
+
+using Combinatorics
+using GSL
+using SpecialFunctions, FastGaussQuadrature
+using LinearAlgebra
+using Requires
+
+import Base: isinf, rand, convert
+import Distributions: ContinuousUnivariateDistribution,
+    ContinuousMatrixDistribution,
+    Beta, Chi,
+    cdf, pdf, entropy, insupport, mean, median, modes, kurtosis, skewness, std, var, moment
+
+export bidrand,    #Generate random bidiagonal matrix
+       tridrand,   #Generate random tridiagonal matrix
+       sprand,     #Generate random sparse matrix
+       eigvalrand, #Generate random set of eigenvalues
+       eigvaljpdf, #Eigenvalue joint probability density
+       cumulant, freecumulant, cf, mgf,
+       cdf, pdf, entropy, insupport, mean, median, modes, kurtosis, skewness, std, var, moment
+
+const Dim2 = Tuple{Int, Int} #Dimensions of a rectangular matrix
+
+#Classical Gaussian matrix ensembles
+include("GaussianEnsembles.jl")
+
+# Classical univariate distributions
+include("densities/Semicircle.jl")
+include("densities/TracyWidom.jl")
+
+# Ginibre
+include("Ginibre.jl")
+
+# determinantal point processes
+include("dpp.jl")
+
+#Generating matrices of Haar measure
+include("Haar.jl")
+include("HaarMeasure.jl")
+
+#Fast histogrammer for matrix eigenvalues - hist_eig
+include("FastHistogram.jl")
+
+#Formal power series
+include("FormalPowerSeries.jl")
+
+#Statistical tests based on random matrix theory
+include("StatisticalTests.jl")
+
+#Stochastic processes
+include("StochasticProcess.jl")
+
+#Invariant ensembles
+function __init__()
+    @require ApproxFun="28f2ccd6-bb30-5033-b560-165f7b14dc2f" begin
+        include("InvariantEnsembles.jl")
+    end
+end
+
+end
